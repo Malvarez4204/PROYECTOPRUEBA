09236f44dc706a1ccb3a6414fd9ae0e5a2e26ae9
Malvarez4204 <106292326+Malvarez4204@users.noreply.github.com>
2022-07-18 20:06:15 -0500

    PROYECTOPRUEBA ADD ARCHIVOS

---
+++ Proyectoprueba/Proyectoprueba/ProyectopruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectopruebaApp.swift
+//  Proyectoprueba
+//
+//  Created by MAC on 17/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ProyectopruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
